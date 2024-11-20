88bb41c8baa26a1f724ac4bc6079e81c921f87a4
Maintrop4940 <urapobizan6@gmail.com>
2024-11-20 18:20:32 +0200

    change name, create script js

---
+++ script.js
@@ -0,0 +1,16 @@
+let gitHub = document.querySelector('.gitHub')
+let inst = document.querySelector('.inst')
+let tg = document.querySelector('.tg')
+
+
+gitHub.addEventListener('.click', function(){
+    location.assign('https://github.com/ljhuygy8y')
+})
+
+inst.addEventListener('.click', function(){
+    location.assign('https://www.instagram.com/')
+})
+
+tg.addEventListener('.click', function(){
+    location.assign('https://web.telegram.org/a/')
+})
